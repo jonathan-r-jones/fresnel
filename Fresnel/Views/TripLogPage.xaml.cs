2a5692ff89841c33b43075331ab7fba5f4cca90b
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-07-05 14:35:48 -0400

    Jul-5-2018_2:35_PM

--- Fresnel/Views/TripLogPage.xaml.cs
+++ Fresnel/Views/TripLogPage.xaml.cs
@@ -39,6 +39,25 @@ namespace Fresnel.Views
                     Date = new DateTime(2017, 2, 5),
                     Latitude = 38.8895,
                     Longitude = -77.0352
+                },
+
+                new TripLogEntry
+                {
+                    Title = "Statue of Liberty",
+                    Notes = "Inspiring!",
+                    Rating = 4,
+                    Date = new DateTime(2017, 4, 13),
+                    Latitude = 40.6892,
+                    Longitude = -74.0444
+                },
+                new TripLogEntry
+                {
+                    Title = "Golden Gate Bridge",
+                    Notes = "Foggy, but beautiful.",
+                    Rating = 5,
+                    Date = new DateTime(2017, 4, 26),
+                    Latitude = 37.8268,
+                    Longitude = -122.4798
                 }
             };
 
