904a60dcd1573f98cfa7920a6b37a831ea23c7dd
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-07-18 11:50:58 -0400

    Jul-18-2018_11:50_AM

--- Fresnel/Views/MonkeyListPage.xaml.cs
+++ Fresnel/Views/MonkeyListPage.xaml.cs
@@ -9,19 +9,28 @@ namespace Fresnel.Views {
         {
             InitializeComponent();
             monkeyListViewModel = new MonkeyListViewModel();
-            ButtonGet.Clicked += async (sender, e) =>
+            // This is the original code. I wanted to get rid of the button. Jul-18-2018
+            //ButtonGet.Clicked += async (sender, e) =>
+            //{
+            //    try
+            //    {
+            //        ButtonGet.IsEnabled = false;
+            //        await monkeyListViewModel.GetMonkeysAsync();
+            //        ButtonGet.IsEnabled = true;
+            //    }
+            //    catch (Exception exception)
+            //    {
+            //        await DisplayAlert(title: "Oh no!", message: "Unable to get monkeys: " + exception, cancel: "OK");
+            //    }
+            //};
+            try
             {
-                try
-                {
-                    ButtonGet.IsEnabled = false;
-                    await monkeyListViewModel.GetMonkeysAsync();
-                    ButtonGet.IsEnabled = true;
-                }
-                catch (Exception exception)
-                {
-                    await DisplayAlert(title: "Oh no!", message: "Unable to get monkeys: " + exception, cancel: "OK");
-                }
-            };
+                monkeyListViewModel.GetMonkeysAsync();
+            }
+            catch (Exception exception)
+            {
+                DisplayAlert(title: "Oh no!", message: "Unable to get monkeys: " + exception, cancel: "OK");
+            }
             List.ItemTapped += async (sender, e) =>
             {
                 var monkey = e.Item;
