1a7e6360a9fbd3bc5faf06a3e8f915dbe05089d3
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-08-07 18:54:00 -0400

    Aug-7-2018_6:54_PM

---
+++ Fresnel/Views/PinchableOnlineImageCS.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace Fresnel.Views
+{
+	public class PinchableOnlineImageCS : ContentPage
+	{
+		public PinchableOnlineImageCS ()
+		{
+			Content = new Grid {
+				Padding = new Thickness (20),
+				Children = {
+					new PinchToZoomContainer {
+						Content = new Image { Source = ImageSource.FromFile ("https://washington-org.s3.amazonaws.com/s3fs-public/metro-updated-map-2017.jpg") }
+					}	
+				}
+			};
+		}
+	}
+}
